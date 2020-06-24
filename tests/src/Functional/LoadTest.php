0ccce91ccbc06e1061f6001b7bddfc74244ed6bd
Serban Bajdechi <serban@lolaent.com>
2020-06-24 15:29:10 +0300

    added functional test for funsion api

--- tests/src/Functional/LoadTest.php
+++ tests/src/Functional/LoadTest.php
@@ -4,13 +4,14 @@ namespace Drupal\Tests\fusion_connector\Functional;
 
 use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\jsonapi\Functional\JsonApiFunctionalTestBase;
 
 /**
  * Simple test to ensure that main page loads with module enabled.
  *
  * @group fusion_connector
  */
-class FusionConnectorTest extends BrowserTestBase {
+class FusionConnectorTest extends JsonApiFunctionalTestBase {
 
   protected $defaultTheme = 'stable';
 
@@ -33,15 +34,27 @@ class FusionConnectorTest extends BrowserTestBase {
    */
   protected function setUp() {
     parent::setUp();
-    $this->user = $this->drupalCreateUser(['administer site configuration']);
+    $this->user = $this->drupalCreateUser(['view fusion_connector node--article'], 'testUser', true);
     $this->drupalLogin($this->user);
   }
 
   /**
    * Tests that the home page loads with a 200 response.
    */
-  public function testLoad() {
-    $this->drupalGet(Url::fromRoute('<front>'));
+  public function testLoadIndex()
+  {
+    $response =  $this->drupalGet('/fusion/');
+    \Drupal::configFactory('fusion_connector.settings')->getEditable('disabled_entities');
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+  /**
+   * Tests that the home page loads with a 200 response.
+   */
+  public function testLoadNodeType()
+  {
+    $response =  $this->drupalGet('/fusion/node/article');
+    \Drupal::configFactory('fusion_connector.settings')->getEditable('disabled_entities');
     $this->assertSession()->statusCodeEquals(200);
   }
 
